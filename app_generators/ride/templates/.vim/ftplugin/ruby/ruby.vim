93a3cfeb82465549e8fc8c9dc22dcda165660f8b
TJ Vanderpoel <bougy.man@gmail.com>
2008-10-14 15:14:33 -0500

    removed dirty file, fixed filtering in StandardError

--- app_generators/ride/templates/.vim/ftplugin/ruby/ruby.vim
+++ app_generators/ride/templates/.vim/ftplugin/ruby/ruby.vim
@@ -2,7 +2,7 @@ fun! LoadRubyTmp()
   let b:fname = substitute(expand("%"), "/", "{slash}", "g")
   let b:filename = "/tmp/ride." . $STY . "->" . $WINDOW . "->" . b:fname
   " echo b:filename
-  execute "write " . b:filename
+  execute "write! " . b:filename
   call system("screen -p 1 -X stuff \"load " . '\"' . b:filename . '\"' . "\n\"")
   call system("screen -X select 1")
 endfun
--- lib/std_err_hooks.rb
+++ lib/std_err_hooks.rb
@@ -1,21 +1,31 @@
 # This hooks into standard error to open backtraces in vim
 class StandardError
   alias _backtrace backtrace
+  def message_is_filtered?
+    matches = message_filters.detect do |message_filter|
+      [self.class, message].join(": ").match message_filter
+    end
+    puts "#{message} Matched #{matches.inspect}" if matches
+    matches ? true : false
+  end
+
   def backtrace
     old = _backtrace
     @old_backtrace ||= nil
     if old.kind_of?(Array) and @old_backtrace.nil?
-      open_vims(old)
+      open_vims(old) unless message_is_filtered?
       @old_backtrace = true
     end
     old
   end
 
   def open_vims(lines)
-    good_lines = lines.select { |l| l.match(/^[\/\w][^:\s]*:\d+(?::.*)?$/) }.map do |line|
+    good_lines = lines.select { |l| l.match(/^[.\/\w][^:\s]*:\d+(?::.*)?$/) }.map do |line|
+      next if backtrace_filters.detect { |n| line.match n }
       file, line_number, rest = line.split(":")
       [file, line_number]
-    end.uniq
+    end.compact.uniq
+    return if good_lines.size == 0
     file, line = good_lines.shift
     if file.match("->")
       window = file.split("->")[1]
@@ -34,5 +44,19 @@ class StandardError
     s << " -c tabnext " if good_lines.size > 0
 =end
     %x{screen -X #{s}}
+  end
+
+  # Don't run the backtrace file opener if the exception matches this
+  def backtrace_filters
+    [
+      "rubygems.rb:\d+:in `activate",
+      "/irb/workspace.rb"
+    ]
+  end
+
+  def message_filters
+    [
+      "Gem::Exception: can't activate"
+    ]
   end
 end
